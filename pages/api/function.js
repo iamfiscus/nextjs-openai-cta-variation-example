211593f583c6e4d7ab6e59493f9d747c9c4cb654
Jonathan Fiscus <jfiscus@p2wtnwhk9k.lan>
2023-07-13 16:19:37 -0400

    add: fucntions openai init

--- pages/api/function.js
+++ pages/api/function.js
@@ -4,8 +4,8 @@ import { roles, openai_function } from '../../utils/prompts'
 
 async function generateImagePrompt(data) {
     const generate = await openai.createImage({
-        prompt: result.image_prompt,
-        n: result.variations.length,
+        prompt: args.image_prompt,
+        n: 1,
         size: '1024x1024',
       })
 }
@@ -23,39 +23,35 @@ export default async function handler(req, res) {
     const openai = new OpenAIApi(configuration)
 
     const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo-0613',
+      model: 'gpt-3.5-turbo-  ',
       messages: prompts,
       functions,
       function_call: 'auto',
     })
     console.log('MESSAGE', completion.data.choices[0].message)
 
-    const response = JSON.parse(completion.data.choices[0].message)
+    const response = completion.data.choices[0].message
 
     if(!response.content && response.function_call.name === 'generateImagePrompt') {
         // Function was reutrned and needs to be called
-        const { name} = response.function_call;
+        const { name } = response.function_call;
         const args = JSON.parse(response.function_call.arguments);
 
-        // const image = await openai.createImage({
-        //     prompt: result.image_prompt,
-        //     n: result.variations.length,
+        // const generate = await openai.createImage({
+        //     prompt: args.image_prompt,
+        //     n: 1,
         //     size: '1024x1024',
         //   })
 
         // OR... call fuction
-        // const generate = generateImagePrompt(data)
+        // const generate = generateImagePrompt(args)
 
         // Let's be fancy and make it dynamic
         const generate = await window[name](args)
 
     }
 
-   
-
-    // TODO mix in images to vairations
-    
-
+    // TODO mixin images to vairations
     const { variations } = result
     const json = { images, variations }
     res.status(200).json(json)
@@ -63,3 +59,5 @@ export default async function handler(req, res) {
     res.status(422).json(error)
   }
 }
+
+
